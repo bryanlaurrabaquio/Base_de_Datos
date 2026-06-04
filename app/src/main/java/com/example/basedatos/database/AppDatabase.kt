9191bd1c3a6dc25a338e0a62056b9cac52393fd2
Alyn Rivera <alyn.ri.ab@gmail.com>
2026-06-03 21:02:07 -0600

    5to commit

---
+++ app/src/main/java/com/example/basedatos/database/AppDatabase.kt
@@ -0,0 +1,14 @@
+package com.example.basedatos.database
+
+import androidx.room.Database
+import androidx.room.RoomDatabase
+import androidx.room.TypeConverters
+import com.example.basedatos.converters.Converters
+import com.example.basedatos.dao.UsuarioDao
+import com.example.basedatos.model.Usuario
+
+@Database(entities = [Usuario::class], version= 1)
+@TypeConverters(Converters::class)
+abstract class AppDatabase: RoomDatabase() {
+    abstract fun usuarioDao(): UsuarioDao
+}
