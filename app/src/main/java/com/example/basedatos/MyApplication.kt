5d1fa89162e0cf3b89c9a7db86004be2b0ad507a
Alyn Rivera <alyn.ri.ab@gmail.com>
2026-06-03 21:13:16 -0600

    8to commit

---
+++ app/src/main/java/com/example/basedatos/MyApplication.kt
@@ -0,0 +1,26 @@
+package com.example.basedatos
+
+import android.app.Application
+import android.content.Context
+import androidx.room.Room
+import com.example.basedatos.database.AppDatabase
+
+class MyApplication : Application() {
+    companion object{
+        @Volatile
+        private var INSTANCE: AppDatabase? = null
+        fun getDatabase(context: Context): AppDatabase{
+            return INSTANCE ?: synchronized(this) {
+                val instance= Room.databaseBuilder(
+                    context.applicationContext,
+                    AppDatabase::class.java,
+                    "usuarios-db"
+                )
+                    .build()
+                INSTANCE = instance
+                instance
+            }
+        }
+    }
+
+}
